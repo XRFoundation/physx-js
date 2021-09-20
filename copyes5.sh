d31ea49f63220aa6c87c8f98e603aa2daba1203d
Josh Field <joshfield999@gmail.com>
2021-09-20 15:00:16 +1000

    lib folder

--- copyes5.sh
+++ copyes5.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp -p ./PhysX/physx/bin/emscripten/release/physx.release.js ../lib/physx.release.cjs.js
+cp -p ./PhysX/physx/bin/emscripten/release/physx.release.js ./lib/physx.release.cjs.js
